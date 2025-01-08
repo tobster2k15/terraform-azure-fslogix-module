c53384f0f3623ea07eb86c7b362e681d85d071f3
tbraunzech <tobias.braun@wf-ib.de>
2025-01-08 07:32:22 +0100

    new delete command

--- resources.tf
+++ resources.tf
@@ -97,9 +97,7 @@ resource "null_resource" "install_az_cli" {
   #### Delete Temp VM via Azure CLI ###
   provisioner "local-exec" {
     command = <<EOF
-    ./env/usr/bin/az vm delete \ --resource-group "${azurerm_resource_group.myrg_shd.name}" \ 
-    --name "${azurerm_windows_virtual_machine.temp_vm_for_st_join.name}" \ 
-    --force-deletion none
+    ./env/usr/bin/az vm delete \ -g "${azurerm_resource_group.myrg_shd.name}" \ -n "${azurerm_windows_virtual_machine.temp_vm_for_st_join.name}" \ --yes
     EOF
   }
   depends_on = [
