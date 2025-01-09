a1667cbcca1226a285cdc6a0516d5fddc9fb4936
tbraunzech <tobias.braun@wf-ib.de>
2025-01-09 11:27:54 +0100

    terrafrom try instead of az

--- resources.tf
+++ resources.tf
@@ -84,19 +84,23 @@ PROTECTED_SETTINGS
 #   SETTINGS
 # }
 
-# resource "null_resource" "install_az_cli" {
-#   provisioner "local-exec" {
-#     command = <<EOF
-#       . /etc/lsb-release
-#       wget https://packages.microsoft.com/repos/azure-cli/pool/main/a/azure-cli/azure-cli_2.36.0-1~$${DISTRIB_CODENAME}_all.deb
-#       mkdir ./env && dpkg -x *.deb ./env
-#       ./env/usr/bin/az login --service-principal -u "${var.ARM_CLIENT_ID}" -p "${var.ARM_CLIENT_SECRET}" -t "${var.ARM_TENANT_ID}"
-#       ./env/usr/bin/az account show
-#       ./env/usr/bin/az account set --subscription ${data.azurerm_subscription.current.subscription_id}
-#       ./env/usr/bin/ az vm delete --resource-group ${azurerm_resource_group.myrg_shd.name} --name ${azurerm_windows_virtual_machine.temp_vm_for_st_join.name} --force-deletion none --yes
-#       ./env/usr/bin/ az storage share create --account-name ${azurerm_storage_account.storage.name} --name fslogix --quota ${var.share_size}
-#     EOF
-#   }
+resource "null_resource" "install_az_cli" {
+  provisioner "local-exec" {
+    command = <<EOF
+      . /etc/lsb-release
+      wget https://packages.microsoft.com/repos/azure-cli/pool/main/a/azure-cli/azure-cli_2.36.0-1~$${DISTRIB_CODENAME}_all.deb
+      mkdir ./env && dpkg -x *.deb ./env
+      ./env/usr/bin/az login --service-principal -u "${var.ARM_CLIENT_ID}" -p "${var.ARM_CLIENT_SECRET}" -t "${var.ARM_TENANT_ID}"
+      ./env/usr/bin/az account show
+      ./env/usr/bin/az account set --subscription ${data.azurerm_subscription.current.subscription_id}
+      ./env/usr/bin/ az vm delete --resource-group ${azurerm_resource_group.myrg_shd.name} --name ${azurerm_windows_virtual_machine.temp_vm_for_st_join.name} --force-deletion none --yes
+      ./env/usr/bin/ az storage share create --account-name ${azurerm_storage_account.storage.name} --name fslogix --quota ${var.share_size}
+    EOF
+  }
+  depends_om  = [
+    azurerm_virtual_machine_extension.domain_join_st
+  ]
+}
 #   #### Delete Temp VM via Azure CLI ###
 #   provisioner "local-exec" {
 #     command = <<EOF
@@ -199,12 +203,12 @@ resource "azurerm_storage_account" "storage" {
 #   }
 }
 
-resource "azurerm_storage_share" "FSShare" {
-  name             = "fslogix"
-  quota            = var.share_size
-  # enabled_protocol = var.share_protocol
-  storage_account_name = azurerm_storage_account.storage.name
-}
+# resource "azurerm_storage_share" "FSShare" {
+#   name             = "fslogix"
+#   quota            = var.share_size
+#   # enabled_protocol = var.share_protocol
+#   storage_account_name = azurerm_storage_account.storage.name
+# }
 
 resource "azurerm_storage_share" "additional_shares" {
   count                = var.additional_shares != null ? var.additional_shares : 0
