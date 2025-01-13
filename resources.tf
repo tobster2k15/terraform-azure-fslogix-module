dc9f54d48994e7bd474436cd9dfe74f85d99dad7
tbraunzech <tobias.braun@wf-ib.de>
2025-01-13 06:52:03 +0100

    test

--- resources.tf
+++ resources.tf
@@ -76,8 +76,9 @@ resource "azurerm_virtual_machine_extension" "st_domain_join" {
   type                 = "CustomScriptExtension"
   type_handler_version = "1.9"
 
-  protected_settings = <<SETTINGS
-  {    
+  settings = <<SETTINGS
+  {
+    fileUris: array(baseScriptUri)
     commandToExecute: 'powershell -ExecutionPolicy Unrestricted -File "$${path.module(scripts/Configuration.ps1)}" ${local.storage_to_domain_script_args} -AdminUserPassword ${var.domain_pass} -verbose'
   }
   SETTINGS
